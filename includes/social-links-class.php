84ccf05094f9cb2d004953be3be8331b366fa55a
cc0a <jmckeeco84@gmail.com>
2019-08-14 15:13:51 -0600

    firm commit

--- includes/social-links-class.php
+++ includes/social-links-class.php
@@ -1,5 +1,4 @@
 <?php
-
 class Social_Links_Widget extends WP_Widget {
 
 	/**
@@ -7,9 +6,9 @@ class Social_Links_Widget extends WP_Widget {
 	 */
 	public function __construct() {
 		parent::__construct(
-			'social_links_widget',
-			__('Social Links Widget', 'sl domain'),
-			array('description' => __('Outputs social icons linked to profiles', 'sl_domain'), )
+			'social_links_widget', // Base ID
+			__( 'Social Links Widget', 'sl_domain' ), // Name
+			array( 'description' => __( 'Outputs social icons linked to profiles', 'sl_domain' ), ) // Args
 		);
 	}
 
@@ -20,9 +19,28 @@ class Social_Links_Widget extends WP_Widget {
 	 * @param array $instance
 	 */
 	public function widget( $args, $instance ) {
-		?>
-		Test Frontend
-		<?php
+		$links = array(
+			'facebook' => esc_attr($instance['facebook_link']),
+			'twitter' => esc_attr($instance['twitter_link']),
+			'linkedin' => esc_attr($instance['linkedin_link']),
+			'google' => esc_attr($instance['google_link'])
+		);
+
+		$icons = array(
+			'facebook' => esc_attr($instance['facebook_icon']),
+			'twitter' => esc_attr($instance['twitter_icon']),
+			'linkedin' => esc_attr($instance['linkedin_icon']),
+			'google' => esc_attr($instance['google_icon'])
+		);
+
+		$icon_width = $instance['icon_width'];
+
+		echo $args['before_widget'];
+
+		// Call Frontend Function
+		$this->getSocialLinks($links, $icons, $icon_width);
+
+		echo $args['after_widget'];
 	}
 
 	/**
@@ -31,10 +49,8 @@ class Social_Links_Widget extends WP_Widget {
 	 * @param array $instance The widget options
 	 */
 	public function form( $instance ) {
-        // Call Form Function
-        $this->getForm($instance); // when calling another function within the same class, use $this
-
-
+		// Call Form Function
+		$this->getForm($instance);
 	}
 
 	/**
@@ -42,63 +58,74 @@ class Social_Links_Widget extends WP_Widget {
 	 *
 	 * @param array $new_instance The new options
 	 * @param array $old_instance The previous options
-	 *
-	 * @return array
 	 */
-	public function update( $new_instance, $old_instance ) { // Update Links for Form
+	public function update( $new_instance, $old_instance ) {
 		// processes widget options to be saved
-        $instance = array(
-          'facebook_link' => (!empty($new_instance['facebook_link'])) ? strip_tags($new_instance['facebook_link']) : '',
-          'twitter_link' => (!empty($new_instance['twitter_link'])) ? strip_tags($new_instance['twitter_link']) : '',
-          'linkedin_link' => (!empty($new_instance['linkedin_link'])) ? strip_tags($new_instance['linkedin_link']) : '',
-          'facebook_icon' => (!empty($new_instance['facebook_icon'])) ? strip_tags($new_instance['facebook_icon']) : '',
-          'twitter_icon' => (!empty($new_instance['twitter_icon'])) ? strip_tags($new_instance['twitter_icon']) : '',
-          'linkedin_icon' => (!empty($new_instance['linkedin_icon'])) ? strip_tags($new_instance['linkedin_icon']) : '',
-          'icon_width' => (!empty($new_instance['icon_width'])) ? strip_tags($new_instance['icon_width']) : ''
-        );
+		$instance = array(
+			'facebook_link' => (!empty($new_instance['facebook_link'])) ? strip_tags($new_instance['facebook_link']) : '',
+			'twitter_link' => (!empty($new_instance['twitter_link'])) ? strip_tags($new_instance['twitter_link']) : '',
+			'linkedin_link' => (!empty($new_instance['linkedin_link'])) ? strip_tags($new_instance['linkedin_link']) : '',
+			'google_link' => (!empty($new_instance['google_link'])) ? strip_tags($new_instance['google_link']) : '',
+			'facebook_icon' => (!empty($new_instance['facebook_icon'])) ? strip_tags($new_instance['facebook_icon']) : '',
+			'twitter_icon' => (!empty($new_instance['twitter_icon'])) ? strip_tags($new_instance['twitter_icon']) : '',
+			'linkedin_icon' => (!empty($new_instance['linkedin_icon'])) ? strip_tags($new_instance['linkedin_icon']) : '',
+			'google_icon' => (!empty($new_instance['google_icon'])) ? strip_tags($new_instance['google_icon']) : '',
+			'icon_width' => (!empty($new_instance['icon_width'])) ? strip_tags($new_instance['icon_width']) : ''
+		);
 
-        return $instance;
-
+		return $instance;
 	}
 
 	/**
-	 * Sets and Displays Form
+	 * Gets and Displays Form
 	 *
 	 * @param array $instance The widget options
 	 */
 	public function getForm( $instance ) {
-	    // Get Facebook Link
-        if(isset($instance['facebook_link'])){ // if facebook link exists
-            $facebook_link = esc_attr($instance['facebook_link']);
-        } else {
-            $facebook_link = 'http://www.facebook.com'; // if no link exists, default to FB homepage
-        }
+		//Get Facebook Link
+		if (isset($instance['facebook_link'])) {
+			$facebook_link = esc_attr($instance['facebook_link']);
+		}
+		else{
+			$facebook_link = 'https://www.facebook.com';
+		}
 
-		// Get Twitter Link
-		if(isset($instance['twitter_link'])){ // if twitter link exists
+		//Get Twitter Link
+		if (isset($instance['twitter_link'])) {
 			$twitter_link = esc_attr($instance['twitter_link']);
-		} else {
-			$twitter_link = 'http://www.twitter.com'; // if no link exists, default to twitter homepage
+		}
+		else{
+			$twitter_link = 'https://www.twitter.com';
 		}
 
-		// Get LinkedIn Link
-		if(isset($instance['linkedin_link'])){ // if linkedin link exists
+		//Get Linkdin Link
+		if (isset($instance['linkedin_link'])) {
 			$linkedin_link = esc_attr($instance['linkedin_link']);
-		} else {
-			$linkedin_link = 'http://www.linkedin.com'; // if no link exists, default to FB homepage
 		}
+		else{
+			$linkedin_link = 'https://www.linkedin.com';
+		}
+
+		//Get Google Link
+		if (isset($instance['google_link'])) {
+			$google_link = esc_attr($instance['google_link']);
+		}
+		else{
+			$google_link = 'https://plus.google.com';
+		}
+
 
 		// ICONS
 
-        // Get Facebook Icon
-        if (isset($instance['facebook_icon'])) {
-            $facebook_icon = esc_attr($instance['facebook_icon']);
-        }
-        else{
-            $facebook_icon = plugins_url() . '/social-links/img/facebook.png';
-        }
+		//Get Facebook Icon
+		if (isset($instance['facebook_icon'])) {
+			$facebook_icon = esc_attr($instance['facebook_icon']);
+		}
+		else{
+			$facebook_icon = plugins_url() . '/social-links/img/facebook.png';
+		}
 
-		// Get Twitter Icon
+		//Get Twitter Icon
 		if (isset($instance['twitter_icon'])) {
 			$twitter_icon = esc_attr($instance['twitter_icon']);
 		}
@@ -106,7 +133,7 @@ class Social_Links_Widget extends WP_Widget {
 			$twitter_icon = plugins_url() . '/social-links/img/twitter.png';
 		}
 
-		// Get LinkedIn Icon
+		//Get Twitter Icon
 		if (isset($instance['linkedin_icon'])) {
 			$linkedin_icon = esc_attr($instance['linkedin_icon']);
 		}
@@ -114,43 +141,86 @@ class Social_Links_Widget extends WP_Widget {
 			$linkedin_icon = plugins_url() . '/social-links/img/linkedin.png';
 		}
 
-		// Get Icon Size
-        if (isset($instance['icon_width'])) {
-            $icon_width = esc_attr($instance['icon_width']);
-        }
-        else{
-            $icon_width = 32;
-        }
+		//Get Google Link
+		if (isset($instance['google_icon'])) {
+			$google_icon = esc_attr($instance['google_icon']);
+		}
+		else{
+			$google_icon = plugins_url() . '/social-links/img/google.png';
+		}
 
-        ?>
+		//Get Icon Size
+		if (isset($instance['icon_width'])) {
+			$icon_width = esc_attr($instance['icon_width']);
+		}
+		else{
+			$icon_width = 32;
+		}
+
+		?>
 
         <h4>Facebook</h4>
         <p>
-        <label for="<?php echo $this->get_field_id('facebook_link'); ?>"><?php _e('Link:'); ?></label>
-           <input class="widefat" id="<?php echo $this->get_field_id('facebook_link'); ?>" name="<?php echo $this->get_field_name('facebook_link'); ?>" type="text" value="<?php echo esc_attr($facebook_link); ?>">
+            <label for="<?php echo $this->get_field_id('facebook_link'); ?>"><?php _e('Link:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('facebook_link'); ?>" name="<?php echo $this->get_field_name('facebook_link'); ?>" type="text" value="<?php echo esc_attr( $facebook_link); ?>">
         </p>
         <p>
-        <label for="<?php echo $this->get_field_id('facebook_icon'); ?>"><?php _e('Icon:'); ?></label>
-           <input class="widefat" id="<?php echo $this->get_field_id('facebook_icon'); ?>" name="<?php echo $this->get_field_name('facebook_icon'); ?>" type="text" value="<?php echo esc_attr($facebook_icon); ?>">
+            <label for="<?php echo $this->get_field_id('facebook_icon'); ?>"><?php _e('Icon:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('facebook_icon'); ?>" name="<?php echo $this->get_field_name('facebook_icon'); ?>" type="text" value="<?php echo esc_attr( $facebook_icon); ?>">
         </p>
+
         <h4>Twitter</h4>
         <p>
-        <label for="<?php echo $this->get_field_id('twitter_link'); ?>"><?php _e('Link:'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('twitter_link'); ?>" name="<?php echo $this->get_field_name('twitter_link'); ?>" type="text" value="<?php echo esc_attr($twitter_link); ?>">
+            <label for="<?php echo $this->get_field_id('twitter_link'); ?>"><?php _e('Link:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('twitter_link'); ?>" name="<?php echo $this->get_field_name('twitter_link'); ?>" type="text" value="<?php echo esc_attr( $twitter_link); ?>">
         </p>
         <p>
-        <label for="<?php echo $this->get_field_id('twitter_icon'); ?>"><?php _e('Icon:'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('twitter_icon'); ?>" name="<?php echo $this->get_field_name('twitter_icon'); ?>" type="text" value="<?php echo esc_attr($twitter_icon); ?>">
+            <label for="<?php echo $this->get_field_id('twitter_icon'); ?>"><?php _e('Icon:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('twitter_icon'); ?>" name="<?php echo $this->get_field_name('twitter_icon'); ?>" type="text" value="<?php echo esc_attr( $twitter_icon); ?>">
         </p>
+
         <h4>LinkedIn</h4>
         <p>
-        <label for="<?php echo $this->get_field_id('linkedin_link'); ?>"><?php _e('Link:'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('linkedin_link'); ?>" name="<?php echo $this->get_field_name('linkedin_link'); ?>" type="text" value="<?php echo esc_attr($linkedin_link); ?>">
+            <label for="<?php echo $this->get_field_id('linkedin_link'); ?>"><?php _e('Link:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('linkedin_link'); ?>" name="<?php echo $this->get_field_name('linkedin_link'); ?>" type="text" value="<?php echo esc_attr( $linkedin_link); ?>">
         </p>
         <p>
-        <label for="<?php echo $this->get_field_id('linkedin_icon'); ?>"><?php _e('Icon:'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('linkedin_icon'); ?>" name="<?php echo $this->get_field_name('linkedin_icon'); ?>" type="text" value="<?php echo esc_attr($linkedin_icon); ?>">
+            <label for="<?php echo $this->get_field_id('linkedin_icon'); ?>"><?php _e('Icon:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('linkedin_icon'); ?>" name="<?php echo $this->get_field_name('linkedin_icon'); ?>" type="text" value="<?php echo esc_attr( $linkedin_icon); ?>">
         </p>
-        <?php
+
+        <h4>Google+</h4>
+        <p>
+            <label for="<?php echo $this->get_field_id('google_link'); ?>"><?php _e('Link:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('google_link'); ?>" name="<?php echo $this->get_field_name('google_link'); ?>" type="text" value="<?php echo esc_attr( $google_link); ?>">
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('google_icon'); ?>"><?php _e('Icon:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('google_icon'); ?>" name="<?php echo $this->get_field_name('google_icon'); ?>" type="text" value="<?php echo esc_attr( $google_icon); ?>">
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id('icon_width'); ?>"><?php _e('Icon Width:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('icon_width'); ?>" name="<?php echo $this->get_field_name('icon_width'); ?>" type="text" value="<?php echo esc_attr($icon_width); ?>">
+        </p>
+		<?php
+	}
+
+	/**
+	 * Gets and Displays Social Icons
+	 *
+	 * @param array $links Social Links
+	 * @param array $icons Social Icons
+	 * @param array $icon_width Width of Icons
+	 */
+	public function getSocialLinks( $links, $icons, $icon_width ) {
+		?>
+        <div class="social-links">
+            <a target="_blank" href="<?php echo esc_attr($links['facebook']); ?>"><img width="<?php echo esc_attr($icon_width); ?>" src="<?php echo esc_attr($icons['facebook']); ?>"></a>
+            <a target="_blank" href="<?php echo esc_attr($links['twitter']); ?>"><img width="<?php echo esc_attr($icon_width); ?>" src="<?php echo esc_attr($icons['twitter']); ?>"></a>
+            <a target="_blank" href="<?php echo esc_attr($links['linkedin']); ?>"><img width="<?php echo esc_attr($icon_width); ?>" src="<?php echo esc_attr($icons['linkedin']); ?>"></a>
+            <a target="_blank" href="<?php echo esc_attr($links['google']); ?>"><img width="<?php echo esc_attr($icon_width); ?>" src="<?php echo esc_attr($icons['google']); ?>"></a>
+        </div>
+		<?php
 	}
 }
